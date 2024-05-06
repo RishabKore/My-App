8fce09f81013240ac624aba7ec4248f1bb2937ce
RishabKore <143122463+RishabKore@users.noreply.github.com>
2024-05-05 21:20:17 -0400

    1

--- app/models/product.py
+++ app/models/product.py
@@ -13,7 +13,7 @@ class Product(BaseModel):
             'name': 'Strawberries',
             'description': 'Juicy organic strawberries.',
             'price': 4.99,
-            'url': 'https://picsum.photos/id/1080/360/200'
+            'url': 'https://cdn.pixabay.com/photo/2016/04/10/15/13/mangoes-1320111_640.jpg'
         },
         {
             'name': 'Cup of Tea',
